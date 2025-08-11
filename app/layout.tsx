cfe11dad82ecfee3d92665c0c53fe84fc8867089
Ali-JF <alijalalifarahani80@gmail.com>
2025-08-11 13:52:48 +0330

    fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import type { Metadata } from "next";
-import "./globals.css";
-import { PropsWithChildren } from "react";
-
-
-const RootLayout:React.FC<PropsWithChildren>=({children})=>{
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-=======
 import "./globals.css";
 import { PropsWithChildren } from "react";
 
@@ -20,7 +7,6 @@ const RootLayout:React.FC<PropsWithChildren>=({children})=>{
         <body>
             {children}
         </body>
->>>>>>> f54ad7991d2fcd1f6a4f51a5430abb45855e5c91
     </html>
   );
 }
